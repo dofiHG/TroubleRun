6428af46f946447ad02a57f86e4f42c01137451b
dofiHG <dofihgsafafafa@gmail.com>
2024-08-04 23:02:38 +0300

    FinishMainMenu

--- TroubleRun/Assets/Scripts/MenuActions.cs
+++ TroubleRun/Assets/Scripts/MenuActions.cs
@@ -7,13 +7,18 @@ using YG;
 public class MenuActions : MonoBehaviour
 {
     public GameObject pausePanel;
+    public Sprite[] languages;
 
     private bool isMenuOpen = false;
+    private int languageInt;
 
     private void Start()
     {
         GameObject.Find("Camera Holder").GetComponent<CameraManager>().mouseSpeed = YandexGame.savesData.sentitivity;
-        GameObject.Find("PercentSecitivityTxT").GetComponent<TMP_Text>().text = $"{YandexGame.savesData.sentitivity.ToString()}%";
+        GameObject.Find("Music").GetComponent<AudioSource>().volume = YandexGame.savesData.musicVolume;
+        GameObject.Find("Music").GetComponent<AudioSource>().volume = YandexGame.savesData.environmentVolume;
+        GameObject.Find("Music").GetComponent<AudioSource>().volume = YandexGame.savesData.effectsVolume;
+        languageInt = YandexGame.savesData.usersLanguage;
     }
 
     private void Update()
@@ -21,13 +26,36 @@ public class MenuActions : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isMenuOpen) { CloseMainMenu(); isMenuOpen = false; }
-            else { pausePanel.SetActive(true); Time.timeScale = 0; isMenuOpen = true; }
+            else 
+            {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0;
+                isMenuOpen = true;
+
+                GameObject.Find("LanguageIMG").GetComponent<Image>().sprite = languages[languageInt];
+
+                EventSystem.current.SetSelectedGameObject(GameObject.Find("SoundSlider"));
+                GameObject.Find("PercentSoundTxT").GetComponent<TMP_Text>().text = $"{YandexGame.savesData.musicVolume}%";
+                GameObject.Find("SoundSlider").GetComponent<Slider>().value = YandexGame.savesData.musicVolume;
+
+                EventSystem.current.SetSelectedGameObject(GameObject.Find("EnvironmestSoundSlider"));
+                GameObject.Find("PercentEnironmentTxT").GetComponent<TMP_Text>().text = $"{YandexGame.savesData.environmentVolume}%";
+                GameObject.Find("EnvironmestSoundSlider").GetComponent<Slider>().value = YandexGame.savesData.environmentVolume;
+
+                EventSystem.current.SetSelectedGameObject(GameObject.Find("EffectsSoundSlider"));
+                GameObject.Find("PercentEffectsTxT").GetComponent<TMP_Text>().text = $"{YandexGame.savesData.effectsVolume}%";
+                GameObject.Find("EffectsSoundSlider").GetComponent<Slider>().value = YandexGame.savesData.effectsVolume;
+
+                EventSystem.current.SetSelectedGameObject(GameObject.Find("SencitivitySlider"));
+                GameObject.Find("PercentSecitivityTxT").GetComponent<TMP_Text>().text = $"{YandexGame.savesData.sentitivity}%";
+                GameObject.Find("SencitivitySlider").GetComponent<Slider>().value = YandexGame.savesData.sentitivity;
+            }
         }
     }
 
     public void ChangeVolume(AudioSource sound)
     {
-        sound.volume = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Slider>().value/100;
+        sound.volume = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Slider>().value / 100;
     }
 
     public void ChangePercentVolume(TMP_Text text)
@@ -37,12 +65,13 @@ public class MenuActions : MonoBehaviour
 
     public void ChangeSencitivity()
     {
-        GameObject.Find("Camera Holder").GetComponent<CameraManager>().mouseSpeed = 
+        GameObject.Find("Camera Holder").GetComponent<CameraManager>().mouseSpeed =
             EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Slider>().value;
     }
 
     public void CloseMainMenu()
     {
+        SaveButton();
         pausePanel.SetActive(false);
         Time.timeScale = 1;
         Cursor.visible = false;
@@ -55,6 +84,15 @@ public class MenuActions : MonoBehaviour
         YandexGame.savesData.environmentVolume = GameObject.Find("EnvironmestSoundSlider").GetComponent<Slider>().value;
         YandexGame.savesData.effectsVolume = GameObject.Find("EffectsSoundSlider").GetComponent<Slider>().value;
         YandexGame.savesData.sentitivity = GameObject.Find("SencitivitySlider").GetComponent<Slider>().value;
+        YandexGame.savesData.usersLanguage = languageInt;
         YandexGame.SaveProgress();
+    }
+
+    public void LanguageChange()
+    {
+        languageInt += 1;
+        if (languageInt > 3) { languageInt = 0; }
+        GameObject.Find("LanguageIMG").GetComponent<Image>().sprite = languages[languageInt];
+        SaveButton();
     }
 }
